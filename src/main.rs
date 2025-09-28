f579cfeb594824e9688d1ca0881a0c7a0ca9484f
Lamaglama39 <lamaglama621@gmail.com>
2025-09-28 21:52:15 +0900

    :sparkles: Change data storage location to SQLite

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,105 @@
-use std::collections::HashMap;
-use std::io::Read;
+use rusqlite::{Connection, Result, params};
+
+#[derive(Debug)]
+struct TodoItem {
+    id: i32,
+    task: String,
+    completed: bool,
+}
 
 struct Todo {
-    // use rust built in HashMap to store key - val pairs
-    map: HashMap<String, bool>,
+    conn: Connection,
 }
 
 impl Todo {
-    fn new() -> Result<Todo, std::io::Error> {
-            // open the db file
-                let mut f = std::fs::OpenOptions::new()
-                    .write(true)
-                    .create(true)
-                    .read(true)
-                    .open("db.txt")?;
-                // read its content into a new string
-                let mut content = String::new();
-                    f.read_to_string(&mut content)?;
+    fn new() -> Result<Todo> {
+        let conn = Connection::open("todo.db")?;
 
-        // allocate an empty HashMap
-        let mut map = HashMap::new();
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                task TEXT NOT NULL,
+                completed BOOLEAN NOT NULL DEFAULT FALSE
+            )",
+            [],
+        )?;
 
-        // loop over each lines of the file
-        for entries in content.lines() {
-            // split and bind values
-            let mut values = entries.split('\t');
-            let key = values.next().expect("No Key");
-            let val = values.next().expect("No Value");
-            // insert them into HashMap
-            map.insert(String::from(key), val.parse().unwrap_or(false));
-        }
-        // Return Ok
-        Ok(Todo { map })
+        Ok(Todo { conn })
     }
 
-    fn complete(&mut self, key: &String) -> Option<()> {
-        match self.map.get_mut(key) {
-            Some(v) => Some(*v = false),
-            None => None,
-        }
+    fn insert(&self, task: String) -> Result<()> {
+        self.conn.execute(
+            "INSERT INTO todos (task, completed) VALUES (?1, ?2)",
+            params![task, false],
+        )?;
+        Ok(())
     }
 
-    fn insert(&mut self, key: String) {
-        // insert a new item into our map.
-        // we pass true as value
-        self.map.insert(key, true);
+    fn complete(&self, task: &str) -> Result<bool> {
+        let rows_affected = self.conn.execute(
+            "UPDATE todos SET completed = TRUE WHERE task = ?1 AND completed = FALSE",
+            params![task],
+        )?;
+        Ok(rows_affected > 0)
     }
 
-    fn list(&self) {
-        for (task, status) in &self.map {
-            let status_text = if *status { "[ ]" } else { "[x]" };
-            println!("{} {}", status_text, task);
-        }
+    fn delete(&self, task: &str) -> Result<bool> {
+        let rows_affected = self.conn.execute(
+            "DELETE FROM todos WHERE task = ?1",
+            params![task],
+        )?;
+        Ok(rows_affected > 0)
     }
 
-    fn delete(&mut self, key: &String) -> Option<()> {
-        match self.map.remove(key) {
-            Some(_) => Some(()),
-            None => None,
-        }
-    }
+    fn list(&self) -> Result<()> {
+        let mut stmt = self.conn.prepare("SELECT id, task, completed FROM todos ORDER BY id")?;
+        let todo_iter = stmt.query_map([], |row| {
+            Ok(TodoItem {
+                id: row.get(0)?,
+                task: row.get(1)?,
+                completed: row.get(2)?,
+            })
+        })?;
 
-    fn save(self) -> Result<(), std::io::Error> {
-        let mut content = String::new();
-        for (k, v) in self.map {
-            let record = format!("{}\t{}\n", k, v);
-            content.push_str(&record)
+        for todo in todo_iter {
+            let todo = todo?;
+            let status_text = if todo.completed { "[x]" } else { "[ ]" };
+            println!("{} {}", status_text, todo.task);
         }
-        std::fs::write("db.txt", content)
+        Ok(())
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
     let action = std::env::args().nth(1).expect("Please specify an action");
 
-    let mut todo = Todo::new().expect("Initialisation of db failed");
+    let todo = Todo::new()?;
 
     if action == "add" {
         let item = std::env::args().nth(2).expect("Please specify an item");
         println!("{:?}, {:?}", action, item);
-        todo.insert(item);
-        match todo.save() {
-            Ok(_) => println!("todo saved"),
-            Err(why) => println!("An error occurred: {}", why),
-        }
+        todo.insert(item)?;
+        println!("todo saved");
     } else if action == "complete" {
         let item = std::env::args().nth(2).expect("Please specify an item");
         println!("{:?}, {:?}", action, item);
-        match todo.complete(&item) {
-            None => println!("'{}' is not present in the list", item),
-            Some(_) => match todo.save() {
-                Ok(_) => println!("todo saved"),
-                Err(why) => println!("An error occurred: {}", why),
-            },
+        if todo.complete(&item)? {
+            println!("todo saved");
+        } else {
+            println!("'{}' is not present in the list or already completed", item);
         }
     } else if action == "list" {
         println!("Todo List:");
-        todo.list();
+        todo.list()?;
     } else if action == "delete" {
         let item = std::env::args().nth(2).expect("Please specify an item");
         println!("{:?}, {:?}", action, item);
-        match todo.delete(&item) {
-            None => println!("'{}' is not present in the list", item),
-            Some(_) => match todo.save() {
-                Ok(_) => println!("todo deleted"),
-                Err(why) => println!("An error occurred: {}", why),
-            },
+        if todo.delete(&item)? {
+            println!("todo deleted");
+        } else {
+            println!("'{}' is not present in the list", item);
         }
     }
+
+    Ok(())
 }
-
-
